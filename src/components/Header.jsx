11d39ff7078330e971bba3476e035260c23fd04e
julius bade <julbade21@gmail.com>
2018-11-16 14:26:25 -0800

    add pictures to webpage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,25 +9,30 @@ function Header() {
       <div>
         <style jsx>{`
           font-family: Helvetica;
-          
-
-          .homeLinks {
-            text-align: right;
-
-            margin-right: 20px;
-          }
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          background-color: #663300;
+          posistion: sticky;
+          top: 0;
+          opacity: 0.7;
+          color: white;
           p {
-            float: right;
-            padding: 20px;
+            margin-top: 25px;
+            margin-right: 100px;
+
           }
+          
+          h2 {
+            margin-left: 50px;
+          }
+
         `}</style>
           <h2>Tap Room</h2>
-            <div className="homeLinks">
-              <p><Link to="/">Home</Link></p>
-              <p><Link to="/beers">Beers</Link></p>
-              <p><Link to="/about">About</Link></p>
+          <p><Link to="/">Home</Link></p>
+          <p><Link to="/beers">Beers</Link></p>
+          <p><Link to="/about">About</Link></p>
 
-          </div>
 
       </div>
     );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import homePicture from '../assets/image/Taproom.jpg';
+import homePicture2 from '../assets/image/Taproom2.jpeg';
+import homePicture3 from '../assets/image/Taproom3.jpg';
 
 
 
@@ -12,15 +14,30 @@ function Home() {
           font-family: Helvetica;
           img {
             width: 100%;
-            height: auto;
+            height: 800px;
           }
         `}</style>
-          <img src={homePicture}/>
+          <img className="mySlides" src={homePicture}/>
+          <img className="mySlides" src={homePicture2}/>
+          <img className="mySlides" src={homePicture3}/>
 
 
       </div>
     );
   }
 
-
   export default Home;
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
+    setTimeout(carousel, 3000);
+}
